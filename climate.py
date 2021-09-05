496bf7aceb6978abc75a1ec6b55a7870fa4eca7f
MightyBOBcnc <mightybob2000@gmail.com>
2021-09-05 05:31:46 -0400

    Renamed functions, and notes
    - Renamed assign_temp to assign_surface_temp for clarity that this is for surface-level temperatures.
    - Renamed assign_humidity to assign_surface_humidity for clarity that this is for surface-level humidity.
    - Fixed a seeming oversight with np.zeros.  From: np.zeros((len(verts),), dtype=np.float32) which has some unneeded parenthesis and comma to np.zeros(len(verts), dtype=np.float32)
    - More notes.

--- climate.py
+++ climate.py
@@ -8,6 +8,7 @@ from util import xyz2latlon, rescale
 # ToDo: Also establish the latitudes of the "tropics" bands of tempreatures (low, mid, high latitudes; tropics, subtropics, polar)
 # ToDo: Factor in axial tilt (tropic of cancer, tropic of capricorn, etc.)  NOTE: latitude bands should probably be defined with a GLOBAL, maybe a dict
 # ToDo: And due to axial tilt, take a time of year as an input to modify values (summer temps are different from winter temps)
+#       The time of year should be set up that it can be any arbitrary amount of time, not a set number like 12, so that we can have planets with any orbital period, and cultures with arbitrary calendar increments (3 months, 60 weeks, 30 months, whatever)
 # Examples:
 # Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
 # -26 -38 -50 -53 -53 -55 -56 -55 -55 -47 -35 -25 South Pole average high in C
@@ -33,9 +34,14 @@ from util import xyz2latlon, rescale
 #   I imagine that a combination of humidity and temperature could produce a crude map like this.  Add in wind speed and atmospheric pressure to improve accuracy.  Maybe also a rate of exchange between air and land (precip and evap) with areas of high exchange.
 
 @njit(cache=True, parallel=True, nogil=True)
-def assign_temp(verts, altitudes):
+def assign_surface_temp(verts, altitudes):
     """Assign starting surface temperatures based on equatorial distance and altitude."""
-    surface_temps = np.zeros((len(verts),), dtype=np.float32)  # ToDo: Temps will only ever be in a range from approximately -60.0 to +60.0 C, so, what's a good dtype for that?  How many points of precision do I need?
+    # ToDo: Temps will only ever be in a range from approximately -60.0 to +60.0 C, so, what's a good dtype for that?  The world record low is approx -90 C, and the record high is approx +56 C.
+    #       float16 could hold that but with little precision. float32 is probably the smallest safe choice.  I also may want room for higher and lower values than Earth produces.
+    #       Consider also that Kelvin and Celsius have the same unit size; an increase of 1 K is identical to an increase of 1 C; the only difference is that C is offset by +273.15 from Kelvin values. (water freezes at 273.15 K and boils at 373.15 K)
+    #       So given that I might want to someday expand the planet generator to non-Earthlike planets, or that I might need temperatures above/below Earth normals, maybe I should store them and calculate them in Kelvin.
+    #       Also the fact that black body radiation is probably always done with kelvin and is tied to temperature and incoming/outgoing energy.
+    surface_temps = np.zeros(len(verts), dtype=np.float32)
 
     # ToDo: Okay, so, temp fallof with altitude is tied to humidity; more humidity = slower falloff.
     # https://www.onthesnow.com/news/a/15157/does-elevation-affect-temperature This says temp falls off at about 9.8C per km in "dry" air, or 6C per km if humidity is 100% (NOTE: This wouldn't hold true for alien planets)
@@ -62,8 +68,10 @@ def assign_temp(verts, altitudes):
 
 # ToDo: Should it be called humidity or moisture? This is the water content of the air.
 # ToDo: Do some book learnin' and find out if altitude modifies humidity. And temperature.
-def assign_humidity(verts, water_mask):
-    humidity = np.zeros((len(verts),), dtype=np.float32)  # ToDo: What's the best dtype for humidity? Humidity can only range from 0-100 but I may want decimals.
+def assign_surface_humidity(verts, water_mask):
+    # ToDo: What's the best dtype for humidity? Humidity can only range from 0-100 but I may want decimals. np.float16 might get me 1 or 2 decimal places, rounded. float32 gets me 5 or 6 decimal places, rounded. uint8 would be more than enough for int values.
+    #       Once I have the climate system working in float32 it might be interesting to go back and change things out for float16 and see how much things change.
+    humidity = np.zeros(len(verts), dtype=np.float32)
 
     return humidity
 
